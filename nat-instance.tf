5efcf51079f48ca2a0c7ad93230d13816fee095a
Vlad <Vlad0Beryozkin@yandex.ru>
2025-08-02 03:48:00 +0300

    final commit

--- nat-instance.tf
+++ nat-instance.tf
@@ -210,7 +210,7 @@ resource "yandex_compute_instance" "nat-instance" {
 
   resources {
     cores         = 2
-    memory        = 2
+    memory        = 4
   }
 
   boot_disk {
@@ -242,7 +242,7 @@ EOF
 
 resource "yandex_vpc_route_table" "nat-instance-route" {
   name       = "nat-instance-route"
-  network_id = yandex_vpc_network.my-vpc.ido
+  network_id = yandex_vpc_network.my-vpc.id
   static_route {
     destination_prefix = "0.0.0.0/0"
     next_hop_address   = yandex_compute_instance.nat-instance.network_interface.0.ip_address
